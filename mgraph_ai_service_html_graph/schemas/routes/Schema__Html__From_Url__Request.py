db0dac7cf1c36e3235f17f1991b77a0d4f33d7d5
Dinis Cruz <dinis.cruz@owasp.org>
2025-12-03 16:57:35 +0000

    started to add support for url-to-html and url-to-dot capabilities

---
+++ mgraph_ai_service_html_graph/schemas/routes/Schema__Html__From_Url__Request.py
@@ -0,0 +1,10 @@
+from osbot_utils.type_safe.primitives.domains.http.safe_str.Safe_Str__Http__User_Agent import Safe_Str__Http__User_Agent
+from osbot_utils.type_safe.primitives.domains.numerical.safe_int.Safe_Int__Positive    import Safe_Int__Positive
+from osbot_utils.type_safe.primitives.domains.web.safe_str.Safe_Str__Url               import Safe_Str__Url
+from osbot_utils.type_safe.Type_Safe                                                   import Type_Safe
+
+
+class Schema__Html__From_Url__Request(Type_Safe):                                                 # Request schema for fetching HTML from URL
+    url         : Safe_Str__Url                                                                   # URL to fetch HTML from
+    timeout     : Safe_Int__Positive  = 30                                                        # Request timeout in seconds
+    user_agent  : Safe_Str__Http__User_Agent  = ''                                                # Optional custom user agent
