3a62525671ecb1b2f390c9e8d9b5fc06ef11db17
Dinis Cruz <dinis.cruz@owasp.org>
2026-01-19 12:35:02 +0000

    added brief for "FLeT REST API - Technical Implementation Brief" updated lambda version

--- mgraph_ai_service_html_graph/config.py
+++ mgraph_ai_service_html_graph/config.py
@@ -4,7 +4,7 @@ SERVICE_NAME                             = package_name
 FAST_API__TITLE                          = "MGraph AI Service Html Graph"
 FAST_API__DESCRIPTION                    = "Base template for MGraph-AI microservices"
 LAMBDA_DEPENDENCIES__HTML_GRAPH__SERVICE = ['httpx==0.28.1'                          ,
-                                            'mgraph-ai-service-cache-client==v0.24.0',
+                                            'mgraph-ai-service-cache-client==v0.27.0',
                                             'mgraph-db==v1.18.0'                     ,
                                             'osbot-fast-api-serverless==v1.31.0'     ]
 
