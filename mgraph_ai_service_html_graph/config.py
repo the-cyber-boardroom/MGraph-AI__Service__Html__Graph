8330a1406d452c848aae2c32ed74ebaefb4f74bf
Dinis Cruz <dinis.cruz@owasp.org>
2026-01-19 12:47:13 +0000

    added memory-fs dependency

--- mgraph_ai_service_html_graph/config.py
+++ mgraph_ai_service_html_graph/config.py
@@ -4,6 +4,7 @@ SERVICE_NAME                             = package_name
 FAST_API__TITLE                          = "MGraph AI Service Html Graph"
 FAST_API__DESCRIPTION                    = "Base template for MGraph-AI microservices"
 LAMBDA_DEPENDENCIES__HTML_GRAPH__SERVICE = ['httpx==0.28.1'                          ,
+                                            'memory-fs==v0.40.0'                     ,
                                             'mgraph-ai-service-cache-client==v0.27.0',
                                             'mgraph-db==v1.18.0'                     ,
                                             'osbot-fast-api-serverless==v1.31.0'     ]
