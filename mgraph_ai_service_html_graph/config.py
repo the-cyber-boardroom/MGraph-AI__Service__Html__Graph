d58768dfb122990ffc7d5deca45f38028fed1480
Dinis Cruz <dinis.cruz@owasp.org>
2026-01-27 22:35:29 +0000

    minor fix to lambda function import

--- mgraph_ai_service_html_graph/config.py
+++ mgraph_ai_service_html_graph/config.py
@@ -6,7 +6,7 @@ FAST_API__DESCRIPTION                    = "Base template for MGraph-AI microser
 LAMBDA_DEPENDENCIES__HTML_GRAPH__SERVICE = ['httpx==0.28.1'                          ,
                                             'memory-fs==v0.40.0'                     ,
                                             'mgraph-ai-service-cache-client==v0.32.0',
-                                            'mgraph-ai-service-cache==0.14.0'        ,
+                                            'mgraph-ai-service-cache==v0.14.0'       ,
                                             'mgraph-db==v1.18.0'                     ,
                                             'osbot-fast-api-serverless==v1.33.0'     ]
 
