c830c740c54fb82fc24306c9b2167b2bc5844426
Dinis Cruz <dinis.cruz@owasp.org>
2026-01-29 23:04:16 +0000

    minor changes

--- mgraph_ai_service_html_graph/utils/deploy/Deploy__Service.py
+++ mgraph_ai_service_html_graph/utils/deploy/Deploy__Service.py
@@ -10,10 +10,10 @@ class Deploy__Service(Deploy__Serverless__Fast_API):
     def deploy_lambda(self):
         with super().deploy_lambda() as _:
             _.add_folder(mgraph_ai_service_html_graph__render_ui.path)
-            _.set_env_variable('URL__TARGET_SERVER__CACHE_SERVICE'            , get_env('AUTH__TARGET_SERVER__CACHE_SERVICE__BASE_URL' )) # BUG , we should not be using this
-            _.set_env_variable('AUTH__TARGET_SERVER__CACHE_SERVICE__BASE_URL' , get_env('AUTH__TARGET_SERVER__CACHE_SERVICE__BASE_URL' ))
-            _.set_env_variable('AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_NAME' , get_env('AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_NAME' ))
-            _.set_env_variable('AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_VALUE', get_env('AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_VALUE' ))
+            # _.set_env_variable('URL__TARGET_SERVER__CACHE_SERVICE'            , get_env('AUTH__TARGET_SERVER__CACHE_SERVICE__BASE_URL' )) # BUG , we should not be using this
+            # _.set_env_variable('AUTH__TARGET_SERVER__CACHE_SERVICE__BASE_URL' , get_env('AUTH__TARGET_SERVER__CACHE_SERVICE__BASE_URL' ))
+            # _.set_env_variable('AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_NAME' , get_env('AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_NAME' ))
+            # _.set_env_variable('AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_VALUE', get_env('AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_VALUE' ))
 
             return _
 
--- tests/deploy_aws/test_Deploy__Service__to__dev.py
+++ tests/deploy_aws/test_Deploy__Service__to__dev.py
@@ -1,8 +1,20 @@
 from unittest                                    import TestCase
+# from osbot_utils.utils.Dev                       import pprint
+# from osbot_utils.utils.Env                       import load_dotenv
+# from osbot_utils.utils.Files                     import path_combine, file_exists
 from tests.deploy_aws.test_Deploy__Service__base import test_Deploy__Service__base
 
 class test_Deploy__Service__to__dev(test_Deploy__Service__base, TestCase):
     stage = 'dev'
 
-    # def test_3__create(self):
-    #     super().test_3__create()
+    # @classmethod
+    # def setUpClass(cls):
+    #     dot_env_file = path_combine(__file__, "../.deploy.env")
+    #     assert file_exists(dot_env_file)
+    #     load_dotenv(dotenv_path=dot_env_file, override=True)
+    #     super().setUpClass()
+    #
+    # def test_4__invoke(self):
+    #     self.deploy_fast_api.create()
+    #     result = self.deploy_fast_api.invoke()
+    #     pprint(result)
